90bd684d52dd07af398383145897b0bdba84c81b
Sam Butler-Thompson <sam@q2q-it.com>
2017-03-07 14:49:42 +0000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var PROD = JSON.parse(process.env.PROD_ENV || '0');
+
+var config = {
+  entry: [
+    'script-loader!jquery/dist/jquery.min.js',
+    'script-loader!foundation-sites/dist/js/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  output: {
+    path: __dirname,
+    filename: PROD ? './public/bundle.min.js' : './public/bundle.js'
+  },
+  resolve: {
+    alias: {
+      Main: path.resolve(__dirname, 'app/components/Main.jsx'),
+      Nav: path.resolve(__dirname, 'app/components/Nav.jsx'),
+      applicationStyles: path.resolve(__dirname, 'app/styles/app.scss')
+    },
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader?presets[]=es2016&presets[]=react&presets[]=stage-0',
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  plugins: PROD !== '0' ? [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ] : [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ]
+};
+
+if(PROD == '0') {
+  config.devtool = 'source-map';
+}
+
+module.exports = config;
