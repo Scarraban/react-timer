843f30e0bb79a39a761cce965f4926d4f7f03d68
Sam Butler-Thompson <sam@q2q-it.com>
2017-03-08 11:33:54 +0000

    Finalize tests and add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -24,6 +24,7 @@ describe('Countdown', () => {
           done();
         }, 1001);
       });
+
       it('should not go below zero when count expires', (done) => {
         var countdown = TestUtils.renderIntoDocument(<Countdown/>);
         countdown._handleSetCountdown(1);
@@ -32,6 +33,30 @@ describe('Countdown', () => {
           expect(countdown.state.count).toBe(0);
           done();
         }, 3001);
+      });
+
+      it('should pause countdown on paused status', (done) => {
+        var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+        countdown._handleSetCountdown(3);
+        countdown._handleStatusChange('paused');
+
+        setTimeout(() => {
+          expect(countdown.state.count).toBe(3);
+          expect(countdown.state.countdownStatus).toBe('paused');
+          done();
+        }, 1001);
+      });
+
+      it('should set count to zero when stopped', (done) => {
+        var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+        countdown._handleSetCountdown(3);
+        countdown._handleStatusChange('stopped');
+
+        setTimeout(() => {
+          expect(countdown.state.count).toBe(0);
+          expect(countdown.state.countdownStatus).toBe('stopped');
+          done();
+        }, 1001);
       });
     });
 });
